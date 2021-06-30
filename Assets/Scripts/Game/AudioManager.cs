bb1ea3e69cab39507633132257440ab265e27014
Thorben0 <thorben.stumpe@gmail.com>
2021-06-30 19:42:58 +0200

    Add audio mixer

--- Assets/Scripts/Game/AudioManager.cs
+++ Assets/Scripts/Game/AudioManager.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] Sounds;
+
+    [SerializeField]
+    private AudioMixer audioMixer;
 
     private void Start()
     {
@@ -15,7 +19,13 @@ public class AudioManager : MonoBehaviour
             soundSource.clip = sound.clip;
             soundSource.volume = sound.volume;
             soundSource.pitch = sound.pitch;
+            soundSource.outputAudioMixerGroup = audioMixer.outputAudioMixerGroup;
         }
+
+        // Apply sound volume from settings to master audio mixer
+        float volume = SettingsManager.Instance.GetVolume();
+        float volumeDd = (float)Math.Log10(Math.Max(0.0001, volume)) * 20;
+        audioMixer.SetFloat("volume", volumeDd);
     }
 
     public void Play(string soundName)
@@ -23,7 +33,7 @@ public class AudioManager : MonoBehaviour
         var s = Array.Find(Sounds, sound => sound.name == soundName);
         s?.audioSource.Play();
     }
-    
+
     public AudioSource GetSound(string soundName)
     {
         var s = Array.Find(Sounds, sound => sound.name == soundName);
