b1e0aa44a06101a03d475b391d9ed1cebe40704b
Nico Franke <nico.franke01@gmail.com>
2021-07-02 01:46:04 +0200

    Finalize mask stealing mechanic™

--- Assets/MaskScript.cs
+++ Assets/MaskScript.cs
@@ -8,8 +8,13 @@ public class MaskScript : MonoBehaviour
     [SerializeField]
     private NpcController _npcController;
 
+    [SerializeField] private GameObject _parentMask;
+
+    private Vector3 _worldPosition;
     private Vector3 _position;
     private Quaternion _rotation;
+
+    private bool _attached;
 
     // Start is called before the first frame update
     void Start()
@@ -19,22 +24,35 @@ public class MaskScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (_attached)
+        {
+            Debug.Log("Rip: " + Vector3.Distance(gameObject.transform.position, _worldPosition) ); 
+        }
 
+        if (_attached && Vector3.Distance(gameObject.transform.position, _worldPosition) > 0.5)
+        {
+            
+            _parentMask.SetActive(false);
+        }
     }
 
     public void OnGrab(SelectEnterEventArgs args)
     {
+        _attached = true;
         Debug.Log("Grabed a Maks!");
         _npcController.StopWalking();
+        _worldPosition = transform.position;
     }
 
     public void OnRelease(SelectExitEventArgs args)
     {
+        _attached = false;
         Debug.Log("Release those Maks!");
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         gameObject.transform.localPosition = _position;
+        gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         gameObject.transform.localRotation = _rotation;
         _npcController.ResumeWalking();
     }
